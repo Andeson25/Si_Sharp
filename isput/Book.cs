6495f9a6311eace2896533e016ce13ad0bfe7c83
kek <oleh.petryk@gmail.com>
2018-06-08 23:59:37 +0300

    first commit

---
+++ isput/Book.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace isput
+{
+    public delegate void bookEvent(String message);
+    public class Book
+    {
+        private static int _globalID = 0;
+        private Int32 _id;
+        private String _name;
+        private String _author;
+        private Double _price;
+        private Boolean _isBought;
+
+       
+
+        public event bookEvent bookHasBeenBought;
+        public event bookEvent bookHasBeenSold;
+
+
+        public String Name
+        {
+            get { return this._name; }
+
+            set { this._name = value; }
+        }
+        public String Author
+        {
+            get { return this._author; }
+            set { this._author = value; }
+        }
+        public Double Price
+        {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+        public Book()
+        {
+            if (_globalID == 0)
+                _id = 1;
+            _globalID += 1;
+            _id = _globalID;
+            _name = "DefaultName";
+            _author = "DefaultAuthor";
+            _price = 0;
+            _isBought = false;
+        }
+        public Book(string name, string author, bool isBought, double price)
+        {
+            if (_globalID == 0)
+                _id = 1;
+            _globalID += 1;
+            _id += 1;
+            this._name = name;
+            this._author = author;
+            this._price = price;
+            this._isBought = isBought;
+        }
+
+        public void buy()
+        {
+            if (bookHasBeenBought != null)
+            {
+                this._isBought = true;
+                this.bookHasBeenBought($"Book has been bought bought for {this._price}");
+                this._price = 0;
+            }
+        }
+        public void sell(Double price)
+        {
+            if (bookHasBeenSold != null)
+            {
+                this._price = price;
+                this._isBought = false;
+                this.bookHasBeenBought($"Book has been sold for {this._price}");
+            }
+        }
+        public static bool operator <(Book left, Book right)
+        {
+            return left.Price < right.Price;
+        }
+
+        public static bool operator >(Book left, Book right)
+        {
+            return left.Price > right.Price;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public void print()
+        {
+            Console.WriteLine("_______________\nid: {0}\nName: {1}\nAuthor: {2}\nIs bought: {3}\nPrice: {4}\n_______________", _id, _name, _author, _isBought, _price);
+        }
+    }
+}
---
+++ isput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace isput
+{
+    class MainClass
+    {
+        public static void Show_Message(String message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void Main(string[] args)
+        {
+            Book book0 = new Book("BookName","BookAuthor",false,500);
+            book0.bookHasBeenBought += Show_Message;
+            book0.bookHasBeenSold += Show_Message;
+            book0.buy();
+            book0.print();
+            book0.sell(1000);
+            book0.print();
+            book0.bookHasBeenBought -= Show_Message;
+            book0.bookHasBeenSold -= Show_Message;
+        }
+
+    }
+}
